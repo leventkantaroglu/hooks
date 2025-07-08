d044e8082129e51bd185f391a8845781b34026cc
Levent Kantaroğlu <63751824+leventkantaroglu@users.noreply.github.com>
2025-07-08 21:54:44 +0300

    Add dummy hooks

---
+++ scripts/hello_world.dart
@@ -0,0 +1,6 @@
+#!/usr/bin/env dart
+
+Future<void> main() async {
+  print('Hello World');
+  exit(0);
+}
---
+++ scripts/no_way.dart
@@ -0,0 +1,6 @@
+#!/usr/bin/env dart
+
+Future<void> main() async {
+  print('No Way');
+  exit(1);
+}
